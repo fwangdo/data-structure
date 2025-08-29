93e514434f266cf8f74b84900d32b8bfae59b965
hdy <35fwangdo@gmail.com>
2025-08-29 19:14:12 +0900

    update

--- js/unionFind.js
+++ js/unionFind.js
@@ -1,20 +1,18 @@
 class UnionFind {
-
-    // TODO: we need to implement rank optimization and coordinate compression. 
-    constructor (N) {
+    constructor(N) {
         this.N = N;  
         this.data = [null]; 
-        for (let i = 1; i <= N; i ++ ) {
-            this.data[i] = i; 
+        // 초기값: -1 (루트이면서 rank=1을 의미)
+        for (let i = 1; i <= N; i++) {
+            this.data[i] = -1; 
         }
-        this.sample = [[1, 3], [3, 4], [5,8], [8,7]]; 
-    }; 
+    }
 
     normalFind(idx) {
-        if (this.data[idx] < 0 ){
+        if (this.data[idx] < 0) {
             return idx; 
         } else {
-            return this.find(this.find(this.data[idx])); 
+            return this.normalFind(this.data[idx]); // 수정: 한 번만 재귀 호출
         }
     }
 
@@ -22,12 +20,43 @@ class UnionFind {
         if (this.data[idx] < 0) {
             return idx; 
         } else {
-            this.data[idx] = this.compFind(this.data[idx]) 
+            this.data[idx] = this.compFind(this.data[idx]); // 경로 압축
             return this.data[idx]; 
         }
     }
 
-    union() {
+    normalUnion(idx1, idx2) {
+        const root1 = this.normalFind(idx1); 
+        const root2 = this.normalFind(idx2); 
+
+        if (root1 === root2) return false; // 이미 같은 집합
+
+        this.data[root2] = root1; // root2를 root1에 합침
+        return true;
+    }
+
+    rankUnion(idx1, idx2) {
+        const root1 = this.compFind(idx1); 
+        const root2 = this.compFind(idx2); 
         
+        if (root1 === root2) return false; // 이미 같은 집합 (수정된 조건)
+        
+        const r1Value = this.data[root1]; // 음수값 (rank)
+        const r2Value = this.data[root2]; // 음수값 (rank)
+        // rank 비교: 더 음수인 쪽이 rank가 높음
+        // 예: -5 < -3 이면 rank 5 > rank 3
+        if (r1Value < r2Value) {
+            // root1의 rank가 더 높으므로 root1이 새로운 root
+            this.data[root2] = root1; 
+        } else if (r1Value > r2Value) {
+            // root2의 rank가 더 높으므로 root2가 새로운 root
+            this.data[root1] = root2; 
+        } else {
+            // rank가 같으면 root1을 root로 하고 rank 증가
+            this.data[root1]--; // rank 증가 (더 음수로)
+            this.data[root2] = root1; 
+        }
+
+        return true; 
     }
 }
